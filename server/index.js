7880d8a17a231726e4382c6022f4bb3f0b1a3cb3
Farhan Shamshad <126975284+Farhan3112@users.noreply.github.com>
2025-06-23 22:45:51 +0530

    Update index.js for vercel

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST, "GET].
+    credentials: true
+  }
+));
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
