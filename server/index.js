5133cfaf1dbbc2f6d53f769f704672d822921569
Farhan Shamshad <126975284+Farhan3112@users.noreply.github.com>
2025-06-24 21:42:13 +0530

    Added env

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -20,10 +22,9 @@ app.use('/', (req,res) => {
   res.send('Hello to Memories API');
 })
 
-const CONNECTION_URL = 'mongodb+srv://memories:memories123@cluster1.fjtabu2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
